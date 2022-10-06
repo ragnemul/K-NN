4530ff6e9e3b9fa5d6f59213cde72bcdb0ffce3b
Luis Menendez <95916489+ragnemul@users.noreply.github.com>
2022-10-06 11:44:21 +0200

    comments added

--- Ejercicio_4.2/IBM-HR-Employee-Attrition.R
+++ Ejercicio_4.2/IBM-HR-Employee-Attrition.R
@@ -12,10 +12,13 @@ data.df = read.csv("https://raw.githubusercontent.com/ragnemul/K-NN/main/Ejercic
 
 data <- subset(data.df, select = -c(EmployeeCount,StandardHours,Over18,EmployeeNumber) )
 
+# Eliminamos los datos nulos
 sum(is.na(data))
 
+# visualizamos el aspecto de los datos
 summary(data.df)
 
+# Mostramos algunas gráficas sobre el contenido de los datos
 library(ggplot2)
 library(grid)
 library(gridExtra)
@@ -33,6 +36,7 @@ StockPlot <- ggplot(data.df,aes(StockOptionLevel,fill = Attrition))+geom_bar()
 
 grid.arrange(agePlot,travelPlot,jobLevelPlot,genderPlot,jobInvPlot,marPlot, ncol=2,numCompPlot, overTimePlot, perfPlot, StockPlot,  top = "Fig 1")
 
+
 # Factorización de valores categóricos
 data$MaritalStatus <- as.factor(data$MaritalStatus)
 data$EducationField <- as.factor(data$EducationField)
@@ -42,6 +46,8 @@ data$Gender <- as.factor(data$Gender)
 data$JobRole <- as.factor(data$JobRole)
 data$OverTime <- as.factor(data$OverTime)
 
+# Fijamos semilla para inicializar datos aleatorios, así podremos obtener 
+#repetitividad en los experimentos
 set.seed(123)
 
 # Particionamiento de los datos en conjuntos de entrenamiento y test
@@ -49,44 +55,63 @@ train_split_idx <- caret::createDataPartition(data$Attrition, p = 0.8, list = FA
 train <- data[train_split_idx, ]
 test <- data[-train_split_idx, ]
 
+#############################
+# primer modelo
 
-fitControl1 <- caret::trainControl(method = "cv", 
-                           number = 10, 
-                           classProbs = TRUE, 
-                           sampling = "smote",
-                           summaryFunction = caret::twoClassSummary,
-                           savePredictions = TRUE)
+# Estrategia de control
+fitControl1 <- caret::trainControl(method = "cv", # validación cruzada
+                           number = 10, # número de paquetes de muestra
+                           classProbs = TRUE, # clasificación
+                           # reduce la muestra de la clase mayoritaria y sintetiza nuevos puntos de datos en la clase minoritaria
+                           sampling = "smote", 
+                           summaryFunction = caret::twoClassSummary, # para optimizar métricas 
+                           savePredictions = TRUE) # guardamos las predicciones
 
-fit_knn1 <- caret::train(Attrition ~ ., 
-                         data=train, 
-                         method="knn",
-                         trControl = fitControl1,	
-                         preProcess = c("range"),			
-                         metric = "Sens",    
-                         tuneGrid = expand.grid(k = 1:50))
+# proceso de entrenamiento
+fit_knn1 <- caret::train(Attrition ~ ., # Clases 
+                         data=train, # datos de entrenamiento
+                         method="knn", # método de estimación
+                         trControl = fitControl1,	# estructura de contorl
+                         preProcess = c("range"),	# normalización de los datos
+                         metric = "Sens",    # métrica que se utilizará para seleccionar el modelo óptimo
+                         tuneGrid = expand.grid(k = 1:50)) # valores a probar de k
 
+# mostramos la gráfica K vs métrica (validación cruzada)
 plot(fit_knn1)
 
-#Make predictions to expose class labels
+# Predicciones para los datos de test usando el modelo
 preds_1 <- predict(fit_knn1, newdata=test, type="raw")
+
+# Obtenemos la matriz de confusión
 confussionMatrix_1 <- caret::confusionMatrix(as.factor(preds_1), as.factor(test$Attrition),positive="Yes")
 
 # Mostramos la matriz de confusión
 draw_2D_confusion_matrix(cm = confussionMatrix_1, caption = "Matriz de confusión test 2")
 
+# primer model
+###########################
 
 
-fitControl2 <- trainControl(method = "repeatedcv", 
+
+#############################
+# segundo modelo
+
+# Estrategia de control
+fitControl2 <- trainControl(method = "repeatedcv", # validaciones cruzadas con repetición
                            number = 5, 
                            repeats = 10,
                            classProbs = TRUE, 
-                           sampling = "down",
+                           # muestreo adicional que se realiza después del remuestreo 
+                           # (normalmente para resolver los desequilibrios de clase).
+                           # sub-conjunto aleatorio de todas las clases en el conjunto de entrenamiento 
+                           # para que sus frecuencias de clase coincidan con la clase menos prevalente
+                           sampling = "down", 
                            summaryFunction = twoClassSummary,
                            savePredictions = TRUE)
 
-
+# proceso de entrenamiento
 fit_knn2 <- caret::train(Attrition ~ ., 
-                        data=train, 
+                        data=train,  
                         method="knn",
                         trControl = fitControl2,	
                         preProcess = c("range"),			
@@ -96,68 +121,39 @@ fit_knn2 <- caret::train(Attrition ~ .,
 
 plot(fit_knn2)
 
-#Make predictions to expose class labels
+# predicción de los datos de test usando el modelo fit_knn2
 preds_2 <- predict(fit_knn2, newdata=test, type="raw")
 confussionMatrix_2 <- caret::confusionMatrix(as.factor(preds_2), as.factor(test$Attrition),positive="Yes")
-
 
 # Mostramos la matriz de confusión
 draw_2D_confusion_matrix(cm = confussionMatrix_2, caption = "Matriz de confusión test 2")
 
+# segundo modelo
+#############################
+
+
+######################################
+# curvas ROC PARA COMPARAR LOS MODELOS
 
 library(plyr)
 library(pROC)
 
+# ROC del primero model
 rocs_fit1 <- llply(unique(fit_knn1$pred$obs), function(cls) {
   roc(response = fit_knn1$pred$obs==cls, predictor = fit_knn1$pred[,as.character(cls)])
   })
 
-
+# ROC del segundo model
 rocs_fit2 <- llply(unique(fit_knn2$pred$obs), function(cls) {
   roc(response = fit_knn2$pred$obs==cls, predictor = fit_knn2$pred[,as.character(cls)])
 })
 
+# Mostramos las curvas ROC
 plot(rocs_fit1[[1]],print.auc = TRUE, print.auc.y = 0.6, col = "red") 
 plot(rocs_fit2[[2]],print.auc = TRUE, print.auc.y = 0.55, col = "blue", add=T, )
 
 
-
-############################################
-# En caso de que necesitemos poner umbrales para asociar a las clases a predecir
-
-pred_prob_fit1 <- predict(fit_knn1, newdata=test, type="prob")
-calc_fit1 <- measureit(score = as.numeric(unlist(pred_prob_fit1["Yes"])), 
-                            class = as.factor(test$Attrition), 
-                            measure = c("TPR", "FPR", "SENS", "SPEC", "PREC", "NPV", "ACC", "FSCR"),
-                            negref = "No")
-
-pred_prob_fit2 <- predict(fit_knn2, newdata=test, type="prob")
-calc_fit2 <- measureit(score = as.numeric(unlist(pred_prob_fit2["Yes"])), 
-                       class = as.factor(test$Attrition), 
-                       measure = c("TPR", "FPR", "SENS", "SPEC", "PREC", "NPV", "ACC", "FSCR"),
-                       negref = "No")
+# curvas ROC PARA COMPARAR LOS MODELOS
+######################################
 
 
-calc_fit <- calc_fit2
-calc_fit <- rapply( calc_fit2, f=function(x) ifelse(is.nan(x),0,x), how="replace" )
-calc_fit <- rapply( calc_fit2, f=function(x) ifelse(is.infinite(x),1,x), how="replace" )
-
-calc <- as.data.frame(do.call(cbind, calc_fit))
-
-FN_cost <- 10
-FP_cost <- 1
-
-calc$cost <- calc$FN * FN_cost + calc$FP * FP_cost
-min.cost.value <- min(calc$cost)
-cost.threshold <- calc$Cutoff[which((calc$cost == min.cost.value))]
-
-threshold.cost.plot <- ggplot(calc, aes(x=Cutoff)) +
-  geom_line(aes(y = cost),   size = 1.5, alpha = 0.5) +
-  labs(x="threshold", y="custom func.", title = "threshold effect in custom func.") +
-  theme(plot.title = element_text(hjust = 0.5))
-
-threshold.cost.plot
-
-threshold.cost.plot + geom_vline(xintercept=cost.threshold, linetype="dashed", color="red")
-
-cat ("Recommended threhold:", round(cost.threshold,2))
